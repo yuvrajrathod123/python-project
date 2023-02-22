a6017514e064402907134ce5d7e1c12e79f62155
yuvrajrathod123 <yuvirathod122@gmail.com>
2023-02-22 21:00:46 +0530

    caregivers system management left side complete

--- caregiver.py
+++ caregiver.py
@@ -66,7 +66,7 @@ class caregiver:
         # ========================  left side label frame ============================
 
         left_frame = LabelFrame(main_frame,bg="white",bd=2,relief=RIDGE,text="Caregiver Details",font=("arial",12,"bold"))
-        left_frame.place(x=10,y=10,width=600,height=580)
+        left_frame.place(x=10,y=10,width=600,height=500)
 
         # img_left=Image.open(r"C:\hello\python-project\images\img2.png")
         # img_left=img_left.resize((580,130),Image.ANTIALIAS)  #ANTILIAS converts high level img to low level
@@ -76,13 +76,169 @@ class caregiver:
         # f_lbl.place(x=5,y=0,width=580,height=130)
 
         # basic details
-        left_frame = LabelFrame(left_frame,bg="white",bd=2,relief=RIDGE,text="Basic Details",font=("arial",12,"bold"))
-        left_frame.place(x=5,y=10,width=585,height=150)
+        basic_details_frame = LabelFrame(left_frame,bg="white",bd=2,relief=RIDGE,text="Basic Details",font=("arial",12,"bold"))
+        basic_details_frame.place(x=5,y=10,width=585,height=110)
 
+        # basic details content
+        # Working mode
+        time_lbl = Label(basic_details_frame,text="Working Mode",font=("arial",10,"bold"),bg='white')
+        time_lbl.grid(row=0,column=0,padx=10)
+
+        time_combo = ttk.Combobox(basic_details_frame,font=("arial",10,"bold"),width=17,state="readonly")
+        time_combo["values"]=("select","Full Time","Part Time")
+        time_combo.current(0)
+        time_combo.grid(row=0,column=1,padx=5,pady=10)
+
+        # Gender
+        gender_lbl = Label(basic_details_frame,text="Gender",font=("arial",10,"bold"),bg='white')
+        gender_lbl.grid(row=0,column=2,padx=10)
+
+        gender_combo = ttk.Combobox(basic_details_frame,font=("arial",10,"bold"),width=17,state="readonly")
+        gender_combo["values"]=("select","Male","Female","Transgender")
+        gender_combo.current(0)
+        gender_combo.grid(row=0,column=3,padx=5,pady=10)
         
+         # Care recipient
+        care_recipient_lbl = Label(basic_details_frame,text="Care Recepient",font=("arial",10,"bold"),bg='white')
+        care_recipient_lbl.grid(row=1,column=0,padx=10)
+
+        care_recipient_combo = ttk.Combobox(basic_details_frame,font=("arial",10,"bold"),width=17,state="readonly")
+        care_recipient_combo["values"]=("select","Elderly(over 60)","Adult with a disability(18 to 59)","child(under 15)")
+        care_recipient_combo.current(0)
+        care_recipient_combo.grid(row=1,column=1,padx=5,pady=10)
+        
+         # Care recipient
+        state_lbl = Label(basic_details_frame,text="State",font=("arial",10,"bold"),bg='white')
+        state_lbl.grid(row=1,column=2,padx=10)
+
+        state_combo = ttk.Combobox(basic_details_frame,font=("arial",10,"bold"),width=17,state="readonly")
+        state_combo["values"]=("select","Maharashtra","Andhra Pradesh","Arunachal Pradesh","Assam","Bihar","Chhattisgarh","Delhi","Goa","Gujarat","Haryana","Himachal Pradesh","Jammu and Kashmir","Jharkhand","Karnataka","Kerala","Ladakh","Madhya Pradesh","Punjab","Rajasthan","Sikkim","Tamil Nadu","Telangana","Uttar Pradesh","Uttarakhand","West Bengal")
+        state_combo.current(0)
+        state_combo.grid(row=1,column=3,padx=5,pady=10)
+        
+     #   ================== Extra information =======================
+        Extra_info_frame = LabelFrame(left_frame,bg="white",bd=2,relief=RIDGE,text="Extra Information",font=("arial",12,"bold"))
+        Extra_info_frame.place(x=5,y=120,width=585,height=350)
+
+        # caregiverId
+        caregiverId_lbl = Label(Extra_info_frame,text="CaregiversID",font=("arial",10,"bold"),bg='white')
+        caregiverId_lbl.grid(row=0,column=0,padx=10,pady=7,sticky=W)
+
+        caregiverId_entry = ttk.Entry(Extra_info_frame,width=20,font=("arial",10,"bold"))
+        caregiverId_entry.grid(row=0,column=1,padx=0,sticky=W)
+
+        # Caregiver name
+        caregiver_name_lbl = Label(Extra_info_frame,text="Caregiver's Name",font=("arial",10,"bold"),bg='white')
+        caregiver_name_lbl.grid(row=0,column=2,padx=10,pady=7,sticky=W)
+
+        caregiver_name_entry = ttk.Entry(Extra_info_frame,width=20,font=("arial",10,"bold"))
+        caregiver_name_entry.grid(row=0,column=3,padx=0,sticky=W)
+
+        # mobile no
+        mobile_no_lbl = Label(Extra_info_frame,text="Mobile No",font=("arial",10,"bold"),bg='white')
+        mobile_no_lbl.grid(row=1,column=0,padx=10,pady=7,sticky=W)
+
+        mobile_no_entry = ttk.Entry(Extra_info_frame,width=20,font=("arial",10,"bold"))
+        mobile_no_entry.grid(row=1,column=1,padx=0,sticky=W)
+
+          # Caregiver email
+        email_lbl = Label(Extra_info_frame,text="Email",font=("arial",10,"bold"),bg='white')
+        email_lbl.grid(row=1,column=2,padx=10,pady=7,sticky=W)
+
+        email_entry = ttk.Entry(Extra_info_frame,width=20,font=("arial",10,"bold"))
+        email_entry.grid(row=1,column=3,padx=0,sticky=W)
+
+        # age
+        age_lbl = Label(Extra_info_frame,text="Age",font=("arial",10,"bold"),bg='white')
+        age_lbl.grid(row=2,column=0,padx=10,pady=7,sticky=W)
+
+        age_entry = ttk.Entry(Extra_info_frame,width=20,font=("arial",10,"bold"))
+        age_entry.grid(row=2,column=1,padx=0,sticky=W)
 
 
-        # right side label frame
+         # date of birth
+        dob_lbl = Label(Extra_info_frame,text="Date Of Birth",font=("arial",10,"bold"),bg='white')
+        dob_lbl.grid(row=2,column=2,padx=10,pady=7,sticky=W)
+
+        dob_entry = ttk.Entry(Extra_info_frame,width=20,font=("arial",10,"bold"))
+        dob_entry.grid(row=2,column=3,padx=0,sticky=W)
+
+
+        # date of joining
+        doj_lbl = Label(Extra_info_frame,text="Date of joining",font=("arial",10,"bold"),bg='white')
+        doj_lbl.grid(row=3,column=0,padx=10,pady=7,sticky=W)
+
+        doj_entry = ttk.Entry(Extra_info_frame,width=20,font=("arial",10,"bold"))
+        doj_entry.grid(row=3,column=1,padx=0,sticky=W)
+
+
+         # city
+        city_lbl = Label(Extra_info_frame,text="City",font=("arial",10,"bold"),bg='white')
+        city_lbl.grid(row=3,column=2,padx=10,pady=7,sticky=W)
+
+        city_entry = ttk.Entry(Extra_info_frame,width=20,font=("arial",10,"bold"))
+        city_entry.grid(row=3,column=3,padx=0,sticky=W)
+
+         # adreess
+        address_lbl = Label(Extra_info_frame,text="Address",font=("arial",10,"bold"),bg='white')
+        address_lbl.grid(row=4,column=0,padx=10,pady=7,sticky=W)
+
+        address_entry = ttk.Entry(Extra_info_frame,width=20,font=("arial",10,"bold"))
+        address_entry.grid(row=4,column=1,padx=0,sticky=W)
+
+
+        # ==================== radio buttton ==============
+
+        radiobtn1 = ttk.Radiobutton(Extra_info_frame,text="Take a photo sample",value="Yes")
+        radiobtn1.grid(row=5,column=0,padx=2,pady=2)
+
+        radiobtn2 = ttk.Radiobutton(Extra_info_frame,text="No photo sample",value="Yes")
+        radiobtn2.grid(row=5,column=1,padx=2,pady=2)
+
+
+        # ==================== button frame ===================
+
+        btn_frame = LabelFrame(Extra_info_frame,bg="white",bd=2,relief=RIDGE)
+        btn_frame.place(x=0,y=210,width=580,height=30)
+
+        save_btn = Button(btn_frame,text="Save",font=("arial",10,"bold"),bg="RoyalBlue1",fg="white",width=17)
+        save_btn.grid(row=0,column=0)
+
+        update_btn = Button(btn_frame,text="Update",font=("arial",10,"bold"),bg="RoyalBlue1",fg="white",width=17)
+        update_btn.grid(row=0,column=1)
+
+        delete_btn = Button(btn_frame,text="Delete",font=("arial",10,"bold"),bg="RoyalBlue1",fg="white",width=17)
+        delete_btn.grid(row=0,column=2)
+
+        reset_btn = Button(btn_frame,text="Reset",font=("arial",10,"bold"),bg="RoyalBlue1",fg="white",width=17)
+        reset_btn.grid(row=0,column=3)
+
+            ################# frame for take a photo button ################
+        btn2_frame = LabelFrame(Extra_info_frame,bg="white",bd=2,relief=RIDGE)
+        btn2_frame.place(x=0,y=240,width=580,height=30)
+
+        take_photo_btn = Button(btn2_frame,text="Take photo sample",font=("arial",10,"bold"),bg="RoyalBlue1",fg="white",width=35)
+        take_photo_btn.grid(row=0,column=0)
+
+        update_photo_btn = Button(btn2_frame,text="Update photo sample",font=("arial",10,"bold"),bg="RoyalBlue1",fg="white",width=35)
+        update_photo_btn.grid(row=0,column=2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # ==================== right side label frame ==================
         right_frame = LabelFrame(main_frame,bg="white",bd=2,relief=RIDGE,text="Caregiver Details",font=("arial",12,"bold"))
         right_frame.place(x=620,y=10,width=625,height=580)
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,2 @@
-img2=.open(r"C:\hello\python-project\images\img1-unsplash.jpg")
-        # img2=img2.resize((500,130),Image.ANTIALIAS)  #ANTILIAS converts high level img to low level
-        # self.photoimg2=ImageTk.PhotoImage(img)
-
-        # f_lbl=Label(self.root,image=self.photoimg)
-        # f_lbl.place(x=600,y=0,width=300,height=130)
+button_frame = LabelFrame(Extra_info_frame,bg="white",bd=2,relief=RIDGE)
+button_frame.place(x=0,y=210,width=580,height=30)
